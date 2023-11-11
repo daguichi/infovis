23d32e9fac7c8c2b6cfdb5a43570c46763a0abe1
Leonardo D'Agostino <ldagostino@itba.edu.ar>
2023-11-11 18:39:54 +0100

    feat: almost done

--- assignment_04/app.js
+++ assignment_04/app.js
@@ -2,34 +2,41 @@
 
 
 function getTutorialInfo() {
-    return {
-        groupNames: "Jane Doe, Max Mustermann",
-        exerciseNum: 4
-    }
+  return {
+    groupNames: "Nikhil Bhavikatti, Athish A Yogi, Leonardo D'Agostino",
+    exerciseNum: 4
+  }
 }
 
 /**
  * Calculates and assigns the sum of the children's weights (recursively) to this node.
- * @param {{ name: String, weight: Number|undefined, children Array|undefined }} node - tree node
+ * @param {{ name: String, weight: Number|undefined, children: Array|undefined }} node - tree node
  */
 function sum_weights(node) {
-    // TODO: insert code here
+  let sum = 0;
+  if (node.children && node.children.length > 0) {
+    node.children.forEach(child => {
+      sum += sum_weights(child);
+    });
+    node.weight = sum;
+  }
+  return node.weight;
 }
 
 /**
  * Returns the maximum depth of the tree node.
- * @param {{ name: String, weight: Number, children Array|undefined }} node - tree node
+ * @param {{ name: String, weight: Number, children: Array|undefined }} node - tree node
  * @returns tree depth
  */
 function getTreeDepth(node) {
-    let depth = 0;
-    node.children?.forEach(child => {
-        const tmpDepth = getTreeDepth(child)
-        if (tmpDepth > depth) {
-            depth = tmpDepth
-        }
-    });
-    return 1 + depth
+  let depth = 0;
+  node.children?.forEach(child => {
+    const tmpDepth = getTreeDepth(child)
+    if (tmpDepth > depth) {
+      depth = tmpDepth
+    }
+  });
+  return 1 + depth
 }
 
 /**
@@ -41,12 +48,31 @@ function getTreeDepth(node) {
  * @param {Number} height - height of the drawing area
  */
 function draw(two, data, width, height) {
-    sum_weights(data);
+  sum_weights(data);
 
-    // prints the data object to the console (interactive)
-    console.log(data);
-    // pretty-prints the data object to the console as a string
-    console.log(JSON.stringify(data, null, 2));
+  const maxDepth = getTreeDepth(data);
 
-    // TODO: insert code here
+  function drawNode(node, x, y, w, h, depth) {
+    const rect = two.makeRectangle(x + w / 2, y + h / 2, w, h);
+    rect.fill = 'red'; // Color for non-leaf nodes
+    const label = two.makeText(node.name, x + w / 2, y + h / 2);
+
+    if (node.children && node.children.length > 0) {
+      let childX = x;
+      node.children.forEach(child => {
+        const childW = (child.weight / node.weight) * w;
+        drawNode(child, childX, y + h, childW, h, depth + 1);
+        childX += childW;
+      });
+    } else {
+      console.log('leaf')
+      rect.fill = 'blue'; // Color for leaf nodes
+    }
+
+    label.rotation = Math.PI / 2; // Rotate label
+  }
+
+  drawNode(data, 0, 0, width, height / maxDepth, 1);
+
+  console.log('done');
 }
